98f9f8e6d614ee565b869281f93cf46c2672888f
Panmax <jiapan.china@gmail.com>
2019-11-05 00:07:21 +0800

    https enable

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-### Go 实现简单的反向代理
-
-参考：[在Go-Gin上使用自簽SSL](https://medium.com/@stancth/%E5%9C%A8go-gin%E4%B8%8A%E4%BD%BF%E7%94%A8%E8%87%AA%E7%B0%BDssl-8100b6ab3c69)
+### Pure Http Reverse Proxy Write By GoLang
 
 ## Usage
 
-`./gin-proxy --remote=192.168.1.101:80 --port=8080`
+`./gin-proxy --remote=192.168.1.101:80 --port=8080 --https=true`
 
 ## Screenshot
 
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 var remoteHost string
 var localPort int
+var httpsEnable bool
 
 var simpleHostProxy = httputil.ReverseProxy{
 	Director: func(req *http.Request) {
@@ -21,11 +22,12 @@ var simpleHostProxy = httputil.ReverseProxy{
 }
 
 func main() {
-	flag.StringVar(&remoteHost,"remote", "", "proxy remote host")
+	flag.StringVar(&remoteHost, "remote", "", "proxy remote host")
 	flag.IntVar(&localPort, "port", 8080, "local port")
+	flag.BoolVar(&httpsEnable, "https", false, "https enable")
 	flag.Parse()
 
-	log.Println(fmt.Sprintf("remoteHost: %s, localPort:%d", remoteHost, localPort))
+	log.Println(fmt.Sprintf("remoteHost: %s, localPort:%d，httpsEnable: %v", remoteHost, localPort, httpsEnable))
 
 	r := gin.Default()
 
@@ -33,7 +35,14 @@ func main() {
 		simpleHostProxy.ServeHTTP(ctx.Writer, ctx.Request)
 	})
 
-	if err := r.RunTLS(fmt.Sprintf(":%d", localPort), "server.crt", "server.key"); err != nil {
+	var err error
+	if httpsEnable {
+		err = r.RunTLS(fmt.Sprintf(":%d", localPort), "server.crt", "server.key")
+	} else {
+		err = r.Run(fmt.Sprintf(":%d", localPort))
+	}
+
+	if err != nil {
 		log.Fatal(err)
 	}
 }
